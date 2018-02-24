dd379719449f8785f81d1fcaa3cb9f61e7590ddf
Polina <polina8kravchuk@gmail.com>
2018-02-24 10:26:55 +0300

    big&small

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+//module.exports = 
+function makeExchange(currency) {
+	if(currency > 10000) {
+		return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	} else if (currency < 0){
+		return {};
+	} else {
+		var H = currency / 50;
+		if ( H >= 1){
+			H = Math.floor(H);
+			this.H = H;
+		}
+
+	}
 }
+
+makeExchange(10001);
